aacc52c2cfb3ea953b1233f6ea1a96e6f72e003f
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2022-03-17 16:25:55 +0000

    Fixed builddll, added string formatting fix and removed -v for verbose compilation. modified .gitignore

--- buildDLL.py
+++ buildDLL.py
@@ -3,4 +3,4 @@ import os
 def buildDLL():
     header_files = os.path.join(os.curdir, "dll/*.h")
     source_files = os.path.join(os.curdir, "dll/*.c")
-    os.system("gcc -v -fPIC -shared -o c_dll.so %s %s" % header_files, source_files)
+    os.system("gcc -fPIC -shared -o c_dll.so %s %s" % (header_files, source_files))
